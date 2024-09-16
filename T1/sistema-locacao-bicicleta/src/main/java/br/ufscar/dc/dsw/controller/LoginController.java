514962664dfc547e0f0b8c16d470f45e51509a95
void-Pedro <pedroborges.itapira@gmail.com>
2024-09-16 14:49:53 -0300

    Update LoginController.java

--- T1/sistema-locacao-bicicleta/src/main/java/br/ufscar/dc/dsw/controller/LoginController.java
+++ T1/sistema-locacao-bicicleta/src/main/java/br/ufscar/dc/dsw/controller/LoginController.java
@@ -55,7 +55,7 @@ public class LoginController extends HttpServlet {
                 HttpSession session = request.getSession();
                 session.setAttribute("locadora", locadora);
                 session.setAttribute("tipoUsuario", "locadora");
-                response.sendRedirect("locadora/dashboard");
+                response.sendRedirect("locadoras/dashboard");
             } else {
                 response.sendRedirect("login.jsp");
             }
